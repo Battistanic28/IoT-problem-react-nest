e060115e9c5bd5c683b9e69c2258fa87509871cd
Nick <n.battista180@gmail.com>
2024-12-28 14:04:13 -0700

    setInterval on server

--- server/src/sensor/sensor.gateway.ts
+++ server/src/sensor/sensor.gateway.ts
@@ -1,34 +1,35 @@
-import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io'
 import { Logger } from "@nestjs/common";
 import { RandomController } from 'src/random/random.controller';
-import { Interval } from '@nestjs/schedule';
 
 @WebSocketGateway({ cors: true })
 export class SensorGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  private intervalId = null
   private logger = new Logger(SensorGateway.name);
   @WebSocketServer() server: Server;
   
   handleConnection(client: Socket) {
     this.logger.log(`Client ID: ${client.id} connected`)
+    this.intervalId = setInterval(() => {
+      const { getRandomNumber } = new RandomController
+      const randomNumber = getRandomNumber()
+      this.logger.log(randomNumber)
+      this.server.emit('randomNumber', randomNumber);
+    }, 5000)
   }
 
   handleDisconnect(client: Socket) {
     this.logger.log(`Client ID: ${client.id} disconnected`)
+    if(this.intervalId) {
+      clearInterval(this.intervalId)
+    }
   }
 
-  @SubscribeMessage('telemetry-data')
-  handleMessage(client: Socket, message: string) {
-    client.emit('message', message)
-  }
-
-  @Interval(5000)
-  sendRandomNumber() {
-    const { getRandomNumber } = new RandomController
-    const randomNumber = getRandomNumber()
-    this.logger.log(randomNumber)
-    this.server.emit('randomNumber', randomNumber);
+  @SubscribeMessage('interval')
+  handleMessage(client: Socket, newInterval: number) {
+    client.emit('interval', newInterval)
   }
 }
